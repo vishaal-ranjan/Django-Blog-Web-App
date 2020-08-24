5572b6d754e71720ed384327d1204ea499bdcfc2
Vishaal Ranjan <vishaal811@gmail.com>
2020-08-24 19:45:48 -0400

    Settings changes

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['mydjangowebapp28.herokuapp.com']
 
